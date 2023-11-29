6901370d47080c3ed011fb98d8cdc40d202c2598
Stian <tiansibo1130@163.com>
2023-11-29 10:08:00 +0800

    Add testlib return status (e.g. _ok) to config

--- src/judger/config.py
+++ src/judger/config.py
@@ -1,3 +1,12 @@
 DEFAULT_TMP_PATH = "/tmp" # This is used to store temporary files, like user's code, test cases, etc.
 IMAGE = "oj"
 suffix = {'C':'.c', 'C++':'.cpp', 'Java':'.java', 'Python':'.py'}
+
+_ok = 0,
+_wa = 1,
+_pe = 2,
+_fail = 3,
+_dirt = 4,
+_points = 5,
+_unexpected_eof = 8,
+_partially = 16
