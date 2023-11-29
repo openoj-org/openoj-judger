13edc5e6e76206eac9aa8ef260a8c6e5e4a52db9
Stian <tiansibo1130@163.com>
2023-11-29 10:08:44 +0800

    clean redundant code and add support for spj

--- src/judger/runner.py
+++ src/judger/runner.py
@@ -4,21 +4,17 @@ import time
 import threading
 import hashlib
 import docker
-from .config import DEFAULT_TMP_PATH, IMAGE
+from .config import DEFAULT_TMP_PATH, IMAGE, _ok, _wa
 from .util import prepare_logger
 
 class Runner:
-    def __init__(self, exe, language, case_id, id, timeout=10, max_memory=1024*1024*128, use_docker=True) -> None:
+    def __init__(self, exe, language, case_id, id, timeout=10, max_memory=1024*1024*128, use_docker=True, use_spj=False, spj_path=None) -> None:
         self.language = language
         self.use_docker = use_docker
-        if self.use_docker:
-            self.exe = os.path.join(DEFAULT_TMP_PATH, str(id), exe)
-            self.input = os.path.join(DEFAULT_TMP_PATH, str(id), f'test_{case_id}.in')
-            self.output = os.path.join(DEFAULT_TMP_PATH, str(id), f'test_{case_id}.out')
-        else:
-            self.exe = os.path.join(DEFAULT_TMP_PATH, str(id), exe)
-            self.input = os.path.join(DEFAULT_TMP_PATH, str(id), f'test_{case_id}.in')
-            self.output = os.path.join(DEFAULT_TMP_PATH, str(id), f'test_{case_id}.out')
+        self.exe = os.path.join(DEFAULT_TMP_PATH, str(id), exe)
+        self.input = os.path.join(DEFAULT_TMP_PATH, str(id), f'test_{case_id}.in')
+        self.output = os.path.join(DEFAULT_TMP_PATH, str(id), f'test_{case_id}.out')
+
         self.timeout = timeout
         self.max_memory = max_memory
         self.use_docker = use_docker
@@ -26,76 +22,64 @@ class Runner:
 
         self.case_id = case_id
         self.id = id
+        self.use_spj = use_spj
+        self.spj_path = os.path.join(DEFAULT_TMP_PATH, str(id), spj_path) if spj_path else None
+        if self.use_spj:
+            assert self.spj_path is not None
+
+        self.status, self.time_usage, self.memory_usage = None, None, None
 
     def run(self):
-        if self.language == 'C++' or self.language == 'C':
-            return self.run_cpp()
-        elif self.language == 'Java':
-            return self.run_java()
-        elif self.language == 'Python':
-            return self.run_python()
-        else:
-            return {'success': False, 'error': 'Language not supported'}
-        
-    def run_cpp(self):
-        cmd = 'ulimit -s {} ; {} < {}'.format(self.max_memory, self.exe, self.input)
-        # cmd = '{} < {}'.format(self.exe, self.input)
-        return self._run(cmd)
-    
-    def run_python(self):
-        cmd = 'ulimit -s {} ; python3 {} < {} '.format(self.max_memory, self.exe, self.input)
-        # cmd = 'python3 {} < {} '.format(self.exe, self.input)
-        return self._run(cmd)
-
-    def _run(self, cmd):
         if not self.use_docker:
             self.logger.info(f'Using the Crunner')
-            # try:
-            #     client = docker.from_env()
-            #     volumes = {DEFAULT_TMP_PATH: {'bind': '/home', 'mode': 'rw'}}
-            #     out = client.containers.run(IMAGE, cmd, volumes=volumes, remove=True)
-            # except docker.errors.ContainerError as e:
-            #     return {'success': False, 'error_type': 'Docker Error','error': e.stderr.decode('utf-8')}
             
-            #Get the absolute path of this file (not include the file name)
             path = os.path.dirname(os.path.abspath(__file__))
             path = os.path.join(path, 'crunner')
             p = subprocess.run(f"{path} {self.exe} {self.language} {self.case_id} {self.id} {self.timeout} {self.max_memory} {self.max_memory}".split())
             self.logger.info(f'returncode: {p.returncode}')
-            with open(f'{DEFAULT_TMP_PATH}/{self.id}/answer_{self.case_id}.out', 'r') as f:
-                out = f.read()
+
             with open(f'{DEFAULT_TMP_PATH}/{self.id}/analysis_{self.case_id}.txt', 'r') as f:
                 analysis = f.read()
-            
-            # 4 lines in analysis, decompose them into 4 vars
+            # 3 lines in analysis, decompose them into 3 vars
             analysis = analysis.rstrip().split('\n')
-            status, time_usage, memory_usage = analysis[0], float(analysis[1]), int(analysis[2])
-            if status != "OK":
-                return {'success': False, 'error_type': status, 'time_usage': time_usage, 'memory_usage': memory_usage}
-        # subprocess ver.-----------------------------------------------------
-        else:
-            self.logger.info(f'Running {cmd} in subprocess')
-            docker_prefix = f"docker run -i -v {DEFAULT_TMP_PATH}:/home {IMAGE} "
-            # p = subprocess.run(docker_prefix + cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, timeout=self.timeout)
-            try:
-                p = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, timeout=self.timeout)
-                err = p.stderr.decode('utf-8')
-                out = p.stdout.decode('utf-8')
-                if p.returncode != 0:
-                    if 'Segmentation fault' in err:
-                        return {'success': False, 'error_type': 'MLE', 'error': err}
-                    return {'success': False, 'error_type': 'MLE','error': err}
-            except subprocess.TimeoutExpired:
-                return {'success': False, 'error_type': 'TLE', 'error': 'Time Limit Exceeded'}
-            except subprocess.CalledProcessError:
-                return {'success': False, 'error_type': 'subprocess error', 'error': 'subprocess.CalledProcessError'}
-        # -----------------------------------------------------
+            self.status, self.time_usage, self.memory_usage = analysis[0], float(analysis[1]), int(analysis[2])
+            if self.status != "OK":
+                return {'success': False, 'error_type': self.status, 'time_usage': self.time_usage, 'memory_usage': self.memory_usage}
+
+            if self.use_spj:
+                return self._spj_run()
+            else:
+                return self._normal_run()
+    
+    def _normal_run(self):
+        with open(f'{DEFAULT_TMP_PATH}/{self.id}/answer_{self.case_id}.out', 'r') as f:
+            out = f.read()
         
         user = hashlib.md5(out.rstrip().encode('utf-8')).hexdigest()
         with open(self.output, 'r') as f:
             answer = hashlib.md5(f.read().rstrip().encode('utf-8')).hexdigest()
         
         if user != answer:
-            return {'success': False, 'error_type':'WA', 'time_usage': time_usage, 'memory_usage': memory_usage}
+            return {'success': False, 'error_type':'WA', 'time_usage': self.time_usage, 'memory_usage': self.memory_usage}
         else:
-            return {'success': True, 'time_usage': time_usage, 'memory_usage': memory_usage}
+            return {'success': True, 'time_usage': self.time_usage, 'memory_usage': self.memory_usage}
+
+    def _spj_run(self):
+        with open(f'{DEFAULT_TMP_PATH}/{self.id}/answer_{self.case_id}.out', 'r') as f:
+            out = f.read()
+        spj_result_path = os.path.join(DEFAULT_TMP_PATH, str(self.id), f'spj_result_{self.case_id}.txt')
+        
+        p = subprocess.run(f"{self.spj_path} {f'{DEFAULT_TMP_PATH}/{self.id}/test_{self.case_id}.in'} {f'{DEFAULT_TMP_PATH}/{self.id}/answer_{self.case_id}.out'} {f'{DEFAULT_TMP_PATH}/{self.id}/test_{self.case_id}.out'} > {spj_result_path}")
+        # TODO check returncode p
+        with open(spj_result_path, 'r') as f:
+            spj_result = f.read()
+        flag = int(spj_result.rstrip().split()[0])
+        if flag == _ok:
+            return {'success': True, 'time_usage': self.time_usage, 'memory_usage': self.memory_usage}
+        elif flag == _wa:
+            return {'success': False, 'error_type':'WA', 'time_usage': self.time_usage, 'memory_usage': self.memory_usage}
+        else:
+            #TODO
+            pass
+        
+        
