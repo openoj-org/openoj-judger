7c038531fbf5f3823ec010b9816cac0aac41f13e
Stian <tiansibo1130@163.com>
2023-11-30 12:46:19 +0800

    Update the setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Make sure you have docker installed and running
-docker build -t oj .
+cd src/judger
+make clean
+make
+cd ../..
 
 # Install judger and its dependencies
 pip3 install .
