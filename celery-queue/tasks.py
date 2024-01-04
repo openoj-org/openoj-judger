3b7586956754a9d72fd4f9eed3756f3eaf860042
Mr_Spade <Mr_Spade@foxmail.com>
2024-01-04 19:54:27 +0800

    add recursion limit

--- celery-queue/tasks.py
+++ celery-queue/tasks.py
@@ -1,16 +1,20 @@
 import os
+import sys
 import time
 from celery import Celery
 from judger.judger import judge_entrance
 
+sys.setrecursionlimit(10**7)
 
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
+CELERY_BROKER_URL = (os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379"),)
+CELERY_RESULT_BACKEND = os.environ.get(
+    "CELERY_RESULT_BACKEND", "redis://localhost:6379"
+)
 
-celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+celery = Celery("tasks", broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
 
-@celery.task(name='tasks.judge_code')
+@celery.task(name="tasks.judge_code")
 def judge_code(data):
     result = judge_entrance(data)
     print(result)
